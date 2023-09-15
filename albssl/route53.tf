1731f979718113a42085a1ad6a767ecdbf20e4fd
symplesims <symplesims@gmail.com>
2023-09-15 15:27:24 +0900

    A record 로 생성해야 함.

--- albssl/route53.tf
+++ albssl/route53.tf
@@ -1,17 +1,22 @@
 resource "aws_route53_record" "this" {
-  name            = module.ctx.domain
-  zone_id         = data.aws_route53_zone.public.zone_id
-  type            = "CNAME"
-  ttl             = "300"
-  records         = [module.alb.lb_dns_name]
-  allow_overwrite = true
+  name    = module.ctx.domain
+  zone_id = data.aws_route53_zone.public.zone_id
+  type    = "A"
+  alias {
+    name                   = module.alb.lb_dns_name
+    zone_id                = module.alb.lb_zone_id
+    evaluate_target_health = true
+  }
+  # allow_overwrite = true
 }
 
 resource "aws_route53_record" "www" {
-  name            = format("www.%s", module.ctx.domain)
-  zone_id         = data.aws_route53_zone.public.zone_id
-  type            = "CNAME"
-  ttl             = "300"
-  records         = [module.alb.lb_dns_name]
-  allow_overwrite = true
+  name    = format("www.%s", module.ctx.domain)
+  zone_id = data.aws_route53_zone.public.zone_id
+  type    = "A"
+  alias {
+    name                   = module.alb.lb_dns_name
+    zone_id                = module.alb.lb_zone_id
+    evaluate_target_health = true
+  }
 }
